61fb3276113e38c8630aacee97f41ec374c9441a
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-06-10 07:26:25 +0300

    created flask web application

--- server/app.py
+++ server/app.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-
+#static route
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return "<h1>Welcome to my page!</h1>"
+#dynamic route
+@app.route('/<string:username>')
+def user_profile(username):
+    return f"<h1>Welcome {username}!</h1>"
+
+#run application as a script
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
